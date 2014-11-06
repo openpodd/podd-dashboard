517c2f86bec65807ae256b3b664cad72e7826657
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2014-11-06 14:28:21 +0700

    Involve mocking redis

--- server/app.js
+++ server/app.js
@@ -9,36 +9,46 @@ var io = require('socket.io')(8888),
 
         Fiber.yield();
     },
+
+    redis = require('redis'),
+
     fs = require('fs'),
     dataset = JSON.parse(fs.readFileSync('../app/api/dashboard.json'));
+
+var producer = redis.createClient(),
+    consumer = redis.createClient();
+
+// Randomly generate village status.
+Fiber(function () {
+    var i = 0, positive, negative, index, data;
+
+    for (;;i++) {
+        // Prepare data.
+        positive = parseInt(Math.random() * 100);
+        negative = parseInt(Math.random() * 100);
+
+        index = parseInt(Math.random() * 10) % 2;
+        data = dataset[index];
+        data.positive = positive;
+        data.negative = negative;
+
+        producer.publish('village:status', JSON.stringify(data));
+
+        sleep(10000 * Math.random());
+    }
+}).run();
+
+consumer.on('message', function (channel, message) {
+    for (i in io.sockets.connected) {
+        io.sockets.connected[i].emit('villageStatus', JSON.parse(message));
+    }
+});
+consumer.subscribe('village:status');
 
 io.on('connection', function (socket) {
     console.log('client connected id:', socket.conn.id,
                 ' from:', socket.handshake.headers.origin);
-
-    var loopRun = function () {
-        Fiber(function () {
-            var i = 0, positive, negative, index, data;
-
-            for (;;i++) {
-                // Prepare data.
-                positive = parseInt(Math.random() * 100);
-                negative = parseInt(Math.random() * 100);
-
-                index = parseInt(Math.random() * 10) % 2;
-                data = dataset[index];
-                data.positive = positive;
-                data.negative = negative;
-
-                socket.emit('villageStatus', data);
-
-                sleep(10000 * Math.random());
-            }
-        }).run();
-    };
-
-    loopRun();
 });
 
 io.serveClient();
-console.log('Listening on 0.0.0.0:8888');
+console.log('Listening on 0.0.0.0:8888 ...');
