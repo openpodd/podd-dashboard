2abd7401c60faec96cb902ea3cf8ec709b56faaa
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2014-11-14 11:35:01 +0700

    Change report gallery styles

--- app/scripts/controllers/report.js
+++ app/scripts/controllers/report.js
@@ -3,5 +3,13 @@
 angular.module('poddDashboardApp')
 
 .controller('ReportViewCtrl', function ($scope) {
+    $scope.$parent.$watch('report', function (newValue) {
+        if (newValue) {
+            $scope.activeImage = $scope.$parent.report.images[0];
+        }
+    });
 
+    $scope.setActiveImage = function (image) {
+        $scope.activeImage = image;
+    };
 });
