9a0b217279900f336a02abc2d61cfb35a87f12df
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2014-12-04 13:46:37 +0700

    Fix page-login class not disappear

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -55,7 +55,7 @@ angular
   ])
 
   // override z-index of ui.bootstrap
-  .run(function ($templateCache) {
+  .run(function ($rootScope, shared, $templateCache) {
     $templateCache.put('template/modal/backdrop.html',
       '<div class=\"modal-backdrop fade {{ backdropClass }}\"\n' +
       '     ng-class=\"{in: animate}\"\n' +
@@ -67,6 +67,8 @@ angular
       '<div tabindex=\"-1\" role=\"dialog\" class=\"modal fade\" ng-class=\"{in: animate}\" ng-style=\"{\'z-index\': 10500 + index*10, display: \'block\'}\" ng-click=\"close($event)\">\n' +
       '    <div class=\"modal-dialog\" ng-class=\"{\'modal-sm\': size == \'sm\', \'modal-lg\': size == \'lg\'}\"><div class=\"modal-content\" modal-transclude></div></div>\n' +
       '</div>');
+
+    $rootScope.shared = shared;
   })
 
   .config(function(cfpLoadingBarProvider) {
