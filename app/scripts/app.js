b9a26f7560aff639d096efaddbecc15c571ca671
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2014-12-04 13:38:22 +0700

    Change image viewer, display as gallery

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -50,8 +50,28 @@ angular
     'angularMoment',
     'ui.bootstrap',
     'angularFileUpload',
-    'perfect_scrollbar'
+    'perfect_scrollbar',
+    'bootstrapLightbox'
   ])
+
+  // override z-index of ui.bootstrap
+  .run(function ($templateCache) {
+    $templateCache.put('template/modal/backdrop.html',
+      '<div class=\"modal-backdrop fade {{ backdropClass }}\"\n' +
+      '     ng-class=\"{in: animate}\"\n' +
+      '     ng-style=\"{\'z-index\': 10400 + (index && 1 || 0) + index*10}\"\n' +
+      '></div>\n' +
+      '');
+
+    $templateCache.put('template/modal/window.html',
+      '<div tabindex=\"-1\" role=\"dialog\" class=\"modal fade\" ng-class=\"{in: animate}\" ng-style=\"{\'z-index\': 10500 + index*10, display: \'block\'}\" ng-click=\"close($event)\">\n' +
+      '    <div class=\"modal-dialog\" ng-class=\"{\'modal-sm\': size == \'sm\', \'modal-lg\': size == \'lg\'}\"><div class=\"modal-content\" modal-transclude></div></div>\n' +
+      '</div>');
+  })
+
+  .config(function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeSpinner = false;
+  })
 
   .factory('addTrailingSlashInterceptor', function ($q) {
     var apiUrls = [
