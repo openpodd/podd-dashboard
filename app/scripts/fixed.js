819be712a19a945a8cc01736b8ddb0fd5fe55f23
Sudarat Yawutthi <sudarat@Opendreams-MacBook-Pro-2.local>
2015-01-08 10:14:20 +0700

    fixed report summary format date

--- app/scripts/fixed.js
+++ app/scripts/fixed.js
@@ -12,3 +12,21 @@ if (moment) {
         monthsShort : 'ม.ค._ก.พ._มี.ค._เม.ย._พ.ค._มิ.ย._ก.ค._ส.ค._ก.ย._ต.ค._พ.ย._ธ.ค.'.split('_'),
     });
 }
+
+
+$(document).ready(function () {
+    $.fn.weekpicker.WeekPicker.prototype.format = function (date) {
+        // Format a Date into a string as specified by RFC 3339.
+        var month = (date.getMonth() + 1).toString(),
+            dom = date.getDate().toString();
+        if (month.length === 1) {
+            month = '0' + month;
+        }
+
+        if (dom.length === 1) {
+            dom = '0' + dom;
+        }
+
+        return dom + "/" + month + '/' + date.getFullYear().toString();
+    }
+});
