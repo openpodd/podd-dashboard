b56b88a45c4088a174fbeb8f56e2acd1ccd13dfb
koyoyo <kotaromaster@hotmail.com>
2015-01-14 14:53:16 +0700

    add button load more reports

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -181,6 +181,18 @@ angular.module('poddDashboardApp')
         }
     });
 
+    // UTILS
+    function QueryStringToJSON(string) {
+        var pairs = string.slice(1).split('&');
+
+        var result = {};
+        pairs.forEach(function(pair) {
+            pair = pair.split('=');
+            result[pair[0]] = decodeURIComponent(pair[1] || '');
+        });
+        return JSON.parse(JSON.stringify(result));
+    }
+
     $scope.loadVillageReports = function (village) {
         var query,
             searcher;
@@ -219,7 +231,61 @@ angular.module('poddDashboardApp')
                 }
             });
 
+            if (items.next) {
+                $scope.loadmoreParams = QueryStringToJSON(items.next);
+            } else {
+                $scope.loadmoreParams = null;
+            }
+
             $scope.reports = items.results;
+        })
+        .catch(function () {
+            $scope.loadingReportListError = true;
+        });
+    };
+
+    $scope.loadmoreVillageReports = function (village, query) {
+        var query = query || {};
+        var searcher;
+
+        if (shared.filterMode) {
+            query = $.extend(query, {
+                q: 'administrationArea:' + village.id + ' AND ' + shared.filterQuery
+            });
+            searcher = Search.query;
+        }
+        else {
+            query = $.extend(query, { administrationArea: village.id });
+            searcher = Reports.list;
+        }
+        // TODO: remove
+        if (shared.rlError) searcher = FailRequest.query;
+
+        return searcher(query).$promise.then(function (items) {
+
+            items.results.forEach(function (item) {
+                // Filter for last 2 weeks
+                if ( isRecentReport(item) ) {
+                    $scope.recentReports.push(item);
+                }
+                // Filter for the older
+                else {
+                    $scope.olderReports = $scope.olderReports.concat([item]);
+                }
+
+                // Check if it is new report and add flag 'isNew'
+                if (shared.newReportQueue[item.id]) {
+                    item.isNew = true;
+                }
+            });
+
+            if (items.next) {
+                $scope.loadmoreParams = QueryStringToJSON(items.next);
+            } else {
+                $scope.loadmoreParams = null;
+            }
+
+            $scope.reports.push.apply(items.results);
         })
         .catch(function () {
             $scope.loadingReportListError = true;
