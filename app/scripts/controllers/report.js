336bdeafbb647b44f765c2c051291ef853762487
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2015-01-20 14:23:14 +0700

    Fix console error noise

--- app/scripts/controllers/report.js
+++ app/scripts/controllers/report.js
@@ -221,9 +221,10 @@ angular.module('poddDashboardApp')
     };
 
     $scope.willShowConfirmationBox = function () {
-        return $scope.flag.current.color !== 'OK' &&
-               $scope.flag.current.color !== 'Case' &&
-               $state.params.confirmCase;
+        return ($scope.flag.current &&
+                $scope.flag.current.color !== 'OK' &&
+                $scope.flag.current.color !== 'Case') &&
+                $state.params.confirmCase;
     };
 
     // Mark as case
