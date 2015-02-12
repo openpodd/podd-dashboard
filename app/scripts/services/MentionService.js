d54d7a2e9ae7606206d1fffa6f7afe572e2c9b3c
Sudarat Yawutthi <sudarat@Opendreams-MacBook-Pro.local>
2015-02-12 13:45:44 +0700

    fix link mention

--- app/scripts/services/MentionService.js
+++ app/scripts/services/MentionService.js
@@ -3,12 +3,12 @@
 angular.module('poddDashboardApp')
 
 .factory('Mentions', function ($resource) {
-    var resource = $resource(config.API_BASEPATH + '/notifications/', {}, {
+    var resource = $resource(config.API_BASEPATH + '/mentions/', {}, {
         get: {
             isArray: true
         },
         seen: {
-            url: config.API_BASEPATH + '/notification/seen/',
+            url: config.API_BASEPATH + '/mentions/seen/',
             method: 'POST',
         }
     });
