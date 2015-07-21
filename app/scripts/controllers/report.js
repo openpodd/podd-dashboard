643db04581eae0b29eb9e84abbfdbc7199a2d46e
icooker811 <icooker811@gmail.com>
2015-07-21 16:24:54 +0700

    fix style

--- app/scripts/controllers/report.js
+++ app/scripts/controllers/report.js
@@ -269,7 +269,7 @@ angular.module('poddDashboardApp')
       var printContents = $('.report-print').html();
       var popupWin = window.open('', '_blank');
       popupWin.document.open();
-      popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="' + window.location.origin + '/styles/app.css" /></head><body onload="window.print()">' + printContents + '</html>');
+      popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="' + window.location.origin + '/styles/app.css" /></head><body>' + printContents + '</html>');
       popupWin.document.close();
     }; 
 })
