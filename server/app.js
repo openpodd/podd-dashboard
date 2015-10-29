1b16c27257d67f9fdc6d7f2806cfc8b8ee872fec
icooker811 <icooker811@gmail.com>
2015-10-29 16:54:04 +0700

    add send apns message

--- server/app.js
+++ server/app.js
@@ -4,14 +4,20 @@ var config = require('./config');
 
 var io = require('socket.io')(8888),
     redis = require('redis'),
+    apns = require('apn'),
     gcm = require('android-gcm'),
     raven = require('raven'),
     client = new raven.Client(config.sentryDSN, { level: 'error' }),
     consumer = redis.createClient();
 
 var pushGCM = function pushGCM(message) {
-    var data = JSON.parse(message),
-        gcmObject = new gcm.AndroidGcm(data.GCMAPIKey),
+    var data = JSON.parse(message);
+    
+    if (data.GCMAPIKey === undefined || 
+        data.androidRegistrationIds === undefined)
+        return;
+
+    var gcmObject = new gcm.AndroidGcm(data.GCMAPIKey),
         gcmMessage = new gcm.Message({
             registration_ids: data.androidRegistrationIds,
             data: {
@@ -49,6 +55,52 @@ var pushGCM = function pushGCM(message) {
     });
 };
 
+var apnsConnection = new apns.Connection({
+    cert: config.apns.key,
+    key: config.apns.key
+});
+
+var pushAPNS = function pushAPNSmessage(message) {
+     // "20344c0e7c1ba256474fc3bbe5b34b76fe5c54c8d534baccdaebab420ee71a7e"
+    var data = JSON.parse(message);
+
+    if (data.apnsRegistrationIds === undefined)
+        return;
+
+    data.apnsRegistrationIds.forEach(function(apnsRegistrationId, i) {
+
+        var apnsDevice = new apns.Device(apnsRegistrationId),
+            apnsMessage = new apns.Notification({
+                expiry: Math.floor(Date.now() / 1000) + config.apns.expiry,
+                badge: 1,
+                sound: 'chime.aiff',
+                alert: data.message,
+                payload: {}
+            });
+
+        apnsConnection.pushNotification(apnsMessage, apnsDevice, function(err) {
+            console.log('ERROR:', err);
+            try {
+                var options = {
+                    extra: {
+                        APNSPayload: apnsMessage
+                    },
+                    tags: {
+                        service: 'apns'
+                    }
+                };
+
+                client.captureError(err, options, function (response) {
+                    console.log('Sentry Error ID :', response.id);
+                });
+            }
+            catch (err) {
+                console.log('- err of err', err);
+            }
+        });
+    });
+};
+
 consumer.on('message', function (channel, message) {
     console.log('New upcoming message from channel: /', channel, '/');
     var i;
@@ -58,6 +110,7 @@ consumer.on('message', function (channel, message) {
 
     if (channel === 'news:new') {
         pushGCM(message);
+        pushAPNS(message);
     }
 
 });
