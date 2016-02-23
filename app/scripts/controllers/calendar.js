774859f3b19bdac095baa9d524b5d71c68b5c72c
icooker811 <icooker811@gmail.com>
2016-02-23 15:36:24 +0700

    fix warning: google calendar white screen

--- app/scripts/controllers/calendar.js
+++ app/scripts/controllers/calendar.js
@@ -3,5 +3,13 @@
 angular.module('poddDashboardApp')
 
 .controller('CalendarModeCtrl', function (Menu) {
-  Menu.setActiveMenu('calendar');
+    Menu.setActiveMenu('calendar');
+    $('#warning').hide();
+    
+    var img = new Image();
+    img.onerror = function () {
+       $('#warning').show();
+    }
+    img.src = "https://accounts.google.com/CheckCookie?continue=https%3A%2F%2Fwww.google.com%2Fintl%2Fen%2Fimages%2Flogos%2Faccounts_logo.png&followup=https%3A%2F%2Fwww.google.com%2Fintl%2Fen%2Fimages%2Flogos%2Faccounts_logo.png&chtml=LoginDoneHtml&checkedDomains=youtube&checkConnection=youtube%3A291%3A1";
+    
 });
