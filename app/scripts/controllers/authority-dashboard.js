c0671f89367e29e919d12c9c177577820b58bc4f
icooker811 <icooker811@gmail.com>
2016-07-04 20:13:14 +0700

    add page: authority dashboard

--- app/scripts/controllers/authority-dashboard.js
+++ app/scripts/controllers/authority-dashboard.js
@@ -1,4 +1,4 @@
-/*global swal */
+/*global swal, moment */
 
 'use strict';
 
@@ -9,6 +9,33 @@ angular.module('poddDashboardApp')
 })
 
 .controller('AuthorityDashboardCtrl', function ($scope, Menu, AdministrationArea,
-      $state, $stateParams, $window, shared) {
+      $state, $stateParams, $window, shared, SummaryAuthorityDashboard) {
 
+    var params = {
+      authority: $stateParams.authorityId,
+      dateStart: $stateParams.dateStart,
+      dateEnd: $stateParams.dateEnd
+    }
+    
+    $scope.startDate = moment($stateParams.dateStart, 'DD/MM/YYYY');
+    $scope.endDate = moment($stateParams.dateEnd, 'DD/MM/YYYY');
+
+    $scope.startDateText = $scope.startDate.format('DD MMM YYYY');
+    $scope.endDateText = $scope.endDate.format('DD MMM YYYY');
+
+    $scope.authority = {};
+    SummaryAuthorityDashboard.get(params).$promise.then(function (data) {
+      $scope.authority = data;
+    });
+
+    var options = {
+      center: [ 18.781516724349704, 98.98681640625 ],
+      zoomLevel: 8,
+      zoomControl: false,
+    };
+    
+    L.mapbox.accessToken = config.MAPBOX_ACCESS_TOKEN;
+    var leafletMap = config.MAPBOX_MAP_ID ?
+                    L.mapbox.map('map', config.MAPBOX_MAP_ID, options) :
+                    L.map('map', options);
 });
