153eba1dd5a8ef32d56698edaa1f0b12c4ab3a71
icooker811 <icooker811@gmail.com>
2016-07-06 13:38:58 +0700

    add marker to map

--- app/scripts/controllers/authority-dashboard.js
+++ app/scripts/controllers/authority-dashboard.js
@@ -16,7 +16,7 @@ angular.module('poddDashboardApp')
       dateStart: $stateParams.dateStart,
       dateEnd: $stateParams.dateEnd
     }
-    
+
     $scope.startDate = moment($stateParams.dateStart, 'DD/MM/YYYY');
     $scope.endDate = moment($stateParams.dateEnd, 'DD/MM/YYYY');
 
@@ -24,18 +24,79 @@ angular.module('poddDashboardApp')
     $scope.endDateText = $scope.endDate.format('DD MMM YYYY');
 
     $scope.authority = {};
-    SummaryAuthorityDashboard.get(params).$promise.then(function (data) {
-      $scope.authority = data;
-    });
+
+    function addMarker(items, markerIcon) {
+      var location = [
+        item.reportLocation.coordinates[1],
+        item.reportLocation.coordinates[0]
+      ];
+
+      var marker = L.marker(location, {
+        icon: markerIcon
+      });
+      marker.addTo(leafletMap);
+    }
 
     var options = {
       center: [ 18.781516724349704, 98.98681640625 ],
       zoomLevel: 8,
       zoomControl: false,
+      dragging: false,
+      touchZoom: false,
+      scrollWheelZoom: false,
+      doubleClickZoom: false
     };
     
     L.mapbox.accessToken = config.MAPBOX_ACCESS_TOKEN;
     var leafletMap = config.MAPBOX_MAP_ID ?
                     L.mapbox.map('map', config.MAPBOX_MAP_ID, options) :
                     L.map('map', options);
+
+    var drawnItems = new L.FeatureGroup();                
+    function addMarker(item, markerIcon) {
+      var location = [
+        item.location.coordinates[1] + (Math.floor(Math.random() * 10) + 1) / 100000,
+        item.location.coordinates[0] + (Math.floor(Math.random() * 10) + 1) / 100000
+      ];
+
+      L.marker(location, {icon: markerIcon}).addTo(drawnItems);
+    }
+
+    SummaryAuthorityDashboard.get(params).$promise.then(function (data) {
+      $scope.authority = data;
+
+
+      // case
+      $scope.authority.cases.administrationAreas.forEach(function(marker) {
+        var greenMarker = L.AwesomeMarkers.icon({
+          icon: '',
+          markerColor: 'green'
+        });
+        addMarker(marker, greenMarker);
+      });
+
+      // suspect-outbreak
+      $scope.authority.suspectOutbreaks.administrationAreas.forEach(function(marker) {
+        var yellowMarker = L.AwesomeMarkers.icon({
+          icon: '',
+          markerColor: 'orange'
+        });
+        
+        addMarker(marker, yellowMarker);
+      });
+
+      // outbreak
+      $scope.authority.outbreaks.administrationAreas.forEach(function(marker) {
+        var redMarker = L.AwesomeMarkers.icon({
+          icon: '',
+          markerColor: 'red'
+        });
+
+        addMarker(marker, redMarker);
+      });
+
+      drawnItems.addTo(leafletMap);
+      leafletMap.fitBounds(drawnItems.getBounds())
+    });
+
 });
