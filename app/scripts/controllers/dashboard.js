1c2a5451cac7330cfcf4b2e0d77e538e5ac546a8
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2016-08-16 20:46:27 +0700

    Fix dashboard performance

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -126,7 +126,10 @@ angular.module('poddDashboardApp')
   var cacheTimeout = 60 * 6;
   var useCache = true;
 
+  var paint = dashboardGraph();
+
   function setDataFromCache(cached) {
+    paint(cached.reportThisWeek);
     $scope.dashboard = cached.visualization;
     $scope.users = cached.newlyReporters;
     $scope.positiveReports = cached.positiveReports;
@@ -166,6 +169,7 @@ angular.module('poddDashboardApp')
         })
         .catch(function (err) {
           // load data with legacy method.
+          loadGraphData();
           loadVolunteersCount();
           loadVolunteersList();
           loadPositiveReports();
@@ -189,6 +193,7 @@ angular.module('poddDashboardApp')
   }
   else {
     // load data with legacy method.
+    loadGraphData();
     loadVolunteersCount();
     loadVolunteersList();
     loadPositiveReports();
@@ -343,6 +348,18 @@ angular.module('poddDashboardApp')
       });
   }
 
+  function loadGraphData() {
+    var params = {
+      period: 'day',
+      subscribe: shared.subscribe,
+      lastWeek: true
+    };
+
+    SummaryReportVisualization.query(params).$promise.then(function (json) {
+      paint(json);
+    });
+  }
+
   $scope.selectedTemplate = function (template) {
     $scope.selectedTemplateContact = template;
     $scope.newSelectedContact = template.contact.to;
@@ -352,3 +369,206 @@ angular.module('poddDashboardApp')
   $scope.selected = 'day';
 
 });
+
+function dashboardGraph() {
+  var colors = d3.scale.category20c();
+
+  var $wrapper = angular.element('#mbars');
+
+  var w = $wrapper.width();
+  var h = $wrapper.height();
+  var padding = {
+    top: 40,
+    right: 40,
+    bottom: 80,
+    left: 60
+  };
+
+  var widthRect = 50;
+
+  var svg = d3.select('#mbars')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h);
+
+  svg.append('g')
+    .attr('class','x axis')
+    .attr('transform','translate(' + padding.left + ',' + (h - padding.bottom) + ')');
+
+  svg.append('g')
+    .attr('class','y axis')
+    .attr('transform','translate(' + padding.left + ',' + padding.top + ')');
+
+  svg.append('text')
+    .attr('transform','rotate(-90)')
+    .attr('y', padding.left - 56)
+    .attr('x', 0-(h/2))
+    .attr('dy','1em')
+    .text('จำนวนรายงาน');
+
+  var groups = svg.selectAll('g')
+    .data([])
+    .enter()
+    .append('g')
+    .attr('class','rgroups')
+    .attr('transform','translate('+ padding.left + ',' + (h - padding.bottom) +')')
+    .style('fill', '#ccc');
+
+  var rects = groups.selectAll('rect')
+    .data(function(d) {
+      return d;
+    })
+    .enter()
+    .append('rect')
+    .attr('width', widthRect)
+    .style('fill-opacity',1e-6);
+
+  var stack = d3.layout.stack();
+
+  var colorHash = {};
+
+  function findById(dataset, id) {
+    var data;
+    dataset.forEach(function (item) {
+      if (item.id === id) {
+        data = item;
+      }
+    });
+    return data;
+  }
+
+  function paint(dataset) {
+    dataset.forEach(function (item, index) {
+      colorHash[item.id] = colors(index);
+      item.colorCode = colors(index);
+    });
+
+    var data = [];
+    dataset.forEach(function (item) {
+      // Find if exists in new dataset, need to do this cuz wanna maintain
+      // bar color and animation.
+      var matchedItem = findById(dataset, item.id);
+      var tmp;
+      if (matchedItem) {
+        data.push(matchedItem.data);
+      }
+      else {
+        tmp = [];
+        item.data.forEach(function (_item) {
+          tmp.push({
+            y: 0,
+            time: _item.time
+          });
+        });
+        data.push(tmp);
+      }
+    });
+    stack(data);
+
+    var xMin = new Date(data[0][0].time);
+    var xMax = new Date(data[0][data[0].length - 1].time);
+    var xScale = d3.time.scale()
+      .domain([xMin, xMax], 8)
+      .rangeRound([0, w - padding.left - padding.right]);
+
+    var yScale = d3.scale.linear()
+      .domain([0,
+        d3.max(data, function(d) {
+          return d3.max(d, function(d) {
+            return d.y0 + d.y;
+          });
+        })
+      ])
+      .range([h-padding.bottom - padding.top, 0]);
+
+    var xAxisRange = d3.time.days;
+
+    var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient('bottom')
+      .ticks(xAxisRange, 1);
+
+    var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient('left')
+      .ticks(10);
+
+    groups = svg.selectAll('.rgroups').data(data);
+    groups.enter().append('g')
+      .attr('class','rgroups')
+      .attr('transform','translate('+ padding.left + ',' + (h - padding.bottom) +')')
+      .style('fill',function(d, i){
+        return colorHash[dataset[i].id];
+      });
+
+    rects = groups.selectAll('rect')
+      .data(function (d) {
+        return d;
+      });
+
+    rects.enter()
+      .append('rect')
+      .attr('x', w)
+      .attr('width', widthRect)
+      .style('fill-opacity', 1e-6);
+
+    rects.transition()
+      .duration(1000)
+      .ease('linear')
+      .attr('x', function(d) {
+        return xScale(new Date(d.time));
+      })
+      .attr('y', function(d) {
+        return -(- yScale(d.y0) - yScale(d.y) + (h - padding.top - padding.bottom)*2);
+      })
+      .attr('height', function(d) {
+        return -yScale(d.y) + (h - padding.top - padding.bottom);
+      })
+      .attr('width', widthRect)
+      .style('fill-opacity', 1);
+
+    rects.exit()
+      .transition()
+      .duration(1000)
+      .ease('circle')
+      .attr('x', w)
+      .remove();
+
+    groups.exit()
+      .transition()
+      .duration(1000)
+      .ease('circle')
+      .attr('x',w)
+      .remove();
+
+    svg.select('.x.axis')
+      .transition()
+      .duration(1000)
+      .ease('circle')
+      .call(xAxis);
+
+    svg.select('.y.axis')
+      .transition()
+      .duration(1000)
+      .ease('circle')
+      .call(yAxis);
+
+    var legend = svg.append('g')
+      .attr('class','legend')
+      .attr('x', w - padding.right - 65)
+      .attr('y', 25)
+      .attr('height', 100)
+      .attr('width',100);
+
+    svg.selectAll('.x.axis text')
+      .attr('style', 'text-anchor:end')
+      .attr('transform', function(d) {
+        var translateX = -this.getBBox().height;
+        var translateY = 12;
+
+        return 'translate(' + translateX + ',' + translateY + ') rotate(-90)';
+      });
+  }
+
+  return paint;
+}
