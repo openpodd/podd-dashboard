6cf491d9a1b172783e9824c2b29c7bae91f5936e
Worawat Yao <worawatyao@gmail.com>
2022-05-12 16:49:18 +0700

    PODD-72 Query real data from api endpoint

--- app/scripts/services/FilterService.js
+++ app/scripts/services/FilterService.js
@@ -1,206 +1,153 @@
 "use strict";
 
 angular
-    .module("poddDashboardApp")
+  .module("poddDashboardApp")
 
-    .factory("Search", function ($resource) {
-        return $resource(
-            config.API_BASEPATH + "/reports/search",
-            {},
-            {
-                query: {
-                    method: "GET",
-                    isArray: false,
-                },
-            }
-        );
-    })
+  .factory("Search", function ($resource) {
+    return $resource(
+      config.API_BASEPATH + "/reports/search",
+      {},
+      {
+        query: {
+          method: "GET",
+          isArray: false,
+        },
+      }
+    );
+  })
 
-    .factory("LocalSearch", function ($resource) {
-        // MOCK data
-        return {
-            query: function (q) {
-                return {
-                    $promise: Promise.resolve({
-                        count: 30,
-                        next: "?page=2",
-                        previous: null,
-                        results: [
-                            {
-                                id: 187282,
-                                reportId: 2,
-                                guid: "0d1345e1-c2b0-48a2-821e-d1e9f01ece9b",
-                                reportTypeId: 77,
-                                reportTypeName: "ไข้เลือดออก",
-                                date: "2017-05-02T09:50:03Z",
-                                administrationAreaId: 114,
-                                negative: true,
-                                incidentDate: "2017-05-02",
-                                createdBy: "6353",
-                                createdByName: "บอม บอม",
-                                createdByThumbnailUrl: null,
-                                flag: "",
-                                testFlag: false,
-                                formDataExplanation:
-                                    "<p>รายงานไข้เลือดออก เลขที่ 00098 ชื่อ e เพศ หญิง อายุ 1\r\nที่ [จังหวัด:จ. เชียงใหม่][อำเภอ:อ. สันป่าตอง][ตำบล:ต. ท่าวังพร้าว] \r\nป่วยเมื่อวันที่ 2017-05-02\r\nเข้ารับการรักษาเมื่อวันที่ 2017-05-02 โรงพยาบาล รพ.กรุงเทพ-เชียงใหม่ \r\n</p>\r\n",
-                                renderedOriginalFormData:
-                                    "<p>รายงานไข้เลือดออก เลขที่ 00098 ชื่อ e เพศ หญิง อายุ 1\r\nที่ [จังหวัด:จ. เชียงใหม่][อำเภอ:อ. สันป่าตอง][ตำบล:ต. ท่าวังพร้าว] \r\nป่วยเมื่อวันที่ 2017-05-02\r\nเข้ารับการรักษาเมื่อวันที่ 2017-05-02 โรงพยาบาล รพ.กรุงเทพ-เชียงใหม่ \r\n</p>\r\n",
-                                administrationAreaAddress: "ซ่อนเร้น",
-                                firstImageThumbnail:
-                                    "https://s3-ap-southeast-1.amazonaws.com/podd/e3e9b46b-9ad9-4334-ad5f-0c8b423be97e-thumbnail",
-                                state: 110,
-                                stateCode: "report",
-                                stateName: "Report",
-                                parent: null,
-                                parentType: null,
-                                tags: null,
-                                reportLocation: [100.5736112, 13.7699096],
-                                commentCount: 2,
-                            },
-                            {
-                                id: 187275,
-                                reportId: 1,
-                                guid: "47d5ad50-4db0-43ad-9237-8aa065c80ee5",
-                                reportTypeId: 77,
-                                reportTypeName: "ไข้เลือดออก",
-                                date: "2017-05-02T09:15:22Z",
-                                administrationAreaId: 114,
-                                negative: true,
-                                incidentDate: "2017-05-02",
-                                createdBy: "6353",
-                                createdByName: "บอม บอม",
-                                createdByThumbnailUrl: null,
-                                flag: "",
-                                testFlag: false,
-                                formDataExplanation:
-                                    "<p>รายงานไข้เลือดออก ddd เพศ หญิง อายุ 1\r\nที่ [จังหวัด:จ. เชียงใหม่][อำเภอ:อ. สารภี][ตำบล:ต. ยางเนิ้ง] \r\nป่วยเมื่อวันที่ 2017-05-02\r\nเข้ารับการรักษาเมื่อวันที่ 2017-05-03 โรงพยาบาล รพ.กรุงเทพ-เชียงใหม่ \r\n</p>\r\n",
-                                renderedOriginalFormData:
-                                    "<p>รายงานไข้เลือดออก ddd เพศ หญิง อายุ 1\r\nที่ [จังหวัด:จ. เชียงใหม่][อำเภอ:อ. สารภี][ตำบล:ต. ยางเนิ้ง] \r\nป่วยเมื่อวันที่ 2017-05-02\r\nเข้ารับการรักษาเมื่อวันที่ 2017-05-03 โรงพยาบาล รพ.กรุงเทพ-เชียงใหม่ \r\n</p>\r\n",
-                                administrationAreaAddress: "ซ่อนเร้น",
-                                firstImageThumbnail:
-                                    "https://s3-ap-southeast-1.amazonaws.com/podd/e6de7784-51b1-4c82-9790-a7ea1dfcd52b-thumbnail",
-                                state: 110,
-                                stateCode: "report",
-                                stateName: "Report",
-                                parent: null,
-                                parentType: null,
-                                tags: null,
-                                reportLocation: [100.5736112, 13.7699096],
-                                commentCount: 3,
-                            },
-                            {
-                                id: 184133,
-                                reportId: 33,
-                                guid: "1070bbb0-6c63-4537-86aa-717f491f1adb",
-                                reportTypeId: 2,
-                                reportTypeName: "สัตว์ป่วย/ตาย",
-                                date: "2017-04-04T02:44:44Z",
-                                administrationAreaId: 114,
-                                negative: true,
-                                incidentDate: "2017-04-04",
-                                createdBy: "896",
-                                createdByName: "",
-                                createdByThumbnailUrl: null,
-                                flag: "",
-                                testFlag: false,
-                                formDataExplanation:
-                                    "<p>พบ หมา[สัตว์กระเพาะเดี่ยว] ป่วยจำนวน 1 ตัว, ตายจำนวน 0 ตัว จากทั้งหมด 20 ตัว จำนวนเล้า/คอกข้างเคียงที่มีอาการ 0\r\n มีอาการดังนี้ ท้องเสีย,ซูบผอม,เสียการทรงตัว\r\n</p>\r\n",
-                                renderedOriginalFormData:
-                                    "<p>พบ หมา[สัตว์กระเพาะเดี่ยว] ป่วยจำนวน 1 ตัว, ตายจำนวน 0 ตัว จากทั้งหมด 20 ตัว จำนวนเล้า/คอกข้างเคียงที่มีอาการ 0\r\n มีอาการดังนี้ ท้องเสีย,ซูบผอม,เสียการทรงตัว\r\n</p>\r\n",
-                                administrationAreaAddress: "ซ่อนเร้น",
-                                firstImageThumbnail: "",
-                                state: 59,
-                                stateCode: "insignificant-report",
-                                stateName: "Insignificant Report",
-                                parent: null,
-                                parentType: null,
-                                tags: null,
-                                reportLocation: null,
-                                commentCount: 10,
-                            },
-                            {
-                                id: 142907,
-                                reportId: 29,
-                                guid: "21a4b398-838d-4547-81b3-f1b0bca29a77",
-                                reportTypeId: 2,
-                                reportTypeName: "สัตว์ป่วย/ตาย",
-                                date: "2016-06-15T11:15:57Z",
-                                administrationAreaId: 114,
-                                negative: true,
-                                incidentDate: "2016-06-15",
-                                createdBy: "339",
-                                createdByName: "Sudarat Yawutthi",
-                                createdByThumbnailUrl:
-                                    "https://podd.s3.amazonaws.com/6229777c-e7cb-43fd-b120-f4ed5fda7129",
-                                flag: "",
-                                testFlag: false,
-                                formDataExplanation:
-                                    "<p>พบ ไก่พื้นเมือง[สัตว์ปีก] ป่วยจำนวน 5 ตัว, ตายจำนวน 0 ตัว จากทั้งหมด 10 ตัว จำนวนเล้า/คอกข้างเคียงที่มีอาการ 0\r\nโดยคาดว่าจะเป็นโรค ห่าไก่ มีอาการดังนี้ ป่วยตายเฉียบพลัน\r\n</p>\r\n",
-                                renderedOriginalFormData:
-                                    "<p>พบ ไก่พื้นเมือง[สัตว์ปีก] ป่วยจำนวน 5 ตัว, ตายจำนวน 0 ตัว จากทั้งหมด 10 ตัว จำนวนเล้า/คอกข้างเคียงที่มีอาการ 0\r\nโดยคาดว่าจะเป็นโรค ห่าไก่ มีอาการดังนี้ ป่วยตายเฉียบพลัน\r\n</p>\r\n",
-                                administrationAreaAddress: "ซ่อนเร้น",
-                                firstImageThumbnail: "",
-                                state: 4,
-                                stateCode: "3",
-                                stateName: "Case",
-                                parent: null,
-                                parentType: null,
-                                tags: null,
-                                reportLocation: [100.5881775, 13.7911403],
-                                commentCount: 3,
-                            },
-                            {
-                                id: 136130,
-                                reportId: 197,
-                                guid: "d3c416b0-2cfc-41cd-93f7-20fccbebebe1",
-                                reportTypeId: 2,
-                                reportTypeName: "สัตว์ป่วย/ตาย",
-                                date: "2016-05-12T08:24:02Z",
-                                administrationAreaId: 114,
-                                negative: true,
-                                incidentDate: "2016-05-12",
-                                createdBy: "896",
-                                createdByName: "",
-                                createdByThumbnailUrl: null,
-                                flag: "",
-                                testFlag: false,
-                                formDataExplanation:
-                                    "<p>พบ ไก่พื้นเมือง[สัตว์ปีก] ป่วยจำนวน 1 ตัว, ตายจำนวน 0 ตัว จากทั้งหมด 20 ตัว จำนวนเล้า/คอกข้างเคียงที่มีอาการ 0\r\n มีอาการดังนี้ ไข่ลดลง,ท้องเสีย,ขี้เหลวมีเลือดปน,คอบิด,ขาเกร็ง,ตาบวมปิด,หน้าบวม,ซึม/เหงา/ไม่กินอาหาร,แคระแกร็น,ไอ จาม,หายใจเสียงดัง\r\n</p>\r\n",
-                                renderedOriginalFormData:
-                                    "<p>พบ ไก่พื้นเมือง[สัตว์ปีก] ป่วยจำนวน 1 ตัว, ตายจำนวน 0 ตัว จากทั้งหมด 20 ตัว จำนวนเล้า/คอกข้างเคียงที่มีอาการ 0\r\n มีอาการดังนี้ ไข่ลดลง,ท้องเสีย,ขี้เหลวมีเลือดปน,คอบิด,ขาเกร็ง,ตาบวมปิด,หน้าบวม,ซึม/เหงา/ไม่กินอาหาร,แคระแกร็น,ไอ จาม,หายใจเสียงดัง\r\n</p>\r\n",
-                                administrationAreaAddress: "ซ่อนเร้น",
-                                firstImageThumbnail: "",
-                                state: 24,
-                                stateCode: "report",
-                                stateName: "Report",
-                                parent: 136128,
-                                parentType: "GENERAL",
-                                tags: null,
-                                reportLocation: [100.5881385, 13.7911665],
-                                commentCount: 0,
-                            },
-                        ],
-                    }),
-                };
-            },
-        };
-        /**
-         * @Query
-         *  - dateFrom: yyyy-mm-dd
-         *  - dateTo: yyyy-mm-dd
-         *  - page: int
-         *  - page_size: int
-         */
-        return $resource(
-            config.API_BASEPATH + "/lgreports/:status",
-            {
-                status: "@status",
-            },
-            {
-                query: {
-                    method: "GET",
-                    isArray: false,
-                },
-            }
-        );
-    });
+  .factory("LocalSearch", function ($resource) {
+    // MOCK data
+    // return {
+    //     query: function (q) {
+    //         return {
+    //             $promise: Promise.resolve({
+    //                 count: 30,
+    //                 next: "?page=2",
+    //                 previous: null,
+    //                 results: [
+    //                     {
+    //     "id": 552459,
+    //     "reportId": 1,
+    //     "guid": "2344efcc-aa7a-4229-b85d-860c58e4bfb0",
+    //     "reportTypeId": 1629,
+    //     "reportTypeName": "Public Incident",
+    //     "reportTypeCategoryCode": "pin",
+    //     "isStateChanged": false,
+    //     "stateCode": "report",
+    //     "stateName": "Report",
+    //     "stateId": 6090,
+    //     "date": "2022-05-12T09:11:26+00:00",
+    //     "createdAt": "2022-05-12T09:12:17.253924+00:00",
+    //     "isAnonymous": false,
+    //     "createdByObject": {
+    //         "id": 26649,
+    //         "name": "tidchu tidchu",
+    //         "username": "podd.26649",
+    //         "firstName": "tidchu",
+    //         "lastName": "tidchu",
+    //         "status": "COORDINATOR",
+    //         "contact": "",
+    //         "avatarUrl": null,
+    //         "thumbnailAvatarUrl": null,
+    //         "authorityAdmins": [],
+    //         "isStaff": false,
+    //         "isSuperuser": false,
+    //         "isAnonymous": false,
+    //         "isPublic": false,
+    //         "domain": 5,
+    //         "administrationArea": null,
+    //         "domainLatitude": 13.7563,
+    //         "domainLongitude": 100.5018
+    //     },
+    //     "incidentDate": "2022-05-12",
+    //     "createdBy": "tidchu tidchu",
+    //     "createdById": 26649,
+    //     "createdByName": "tidchu tidchu",
+    //     "administrationAreaId": 2397,
+    //     "formData": {
+    //         "incident": "ไฟส่องสว่างชำรุด",
+    //         "reportTypeVersion": 2,
+    //         "programVersion": 104,
+    //         "reporter": "แจ้งเหตุด้วยตนเอง (ระบบดึงออโต้)"
+    //     },
+    //     "originalFormData": {
+    //         "incident": "ไฟส่องสว่างชำรุด",
+    //         "reportTypeVersion": 2,
+    //         "programVersion": 104,
+    //         "reporter": "แจ้งเหตุด้วยตนเอง (ระบบดึงออโต้)"
+    //     },
+    //     "negative": true,
+    //     "testFlag": false,
+    //     "createdByTelephone": "0891124242",
+    //     "createdByProjectMobileNumber": null,
+    //     "createdByThumbnailUrl": null,
+    //     "parent": null,
+    //     "parentType": null,
+    //     "reportLocation": {
+    //         "type": "Point",
+    //         "coordinates": [
+    //             100.5647297,
+    //             13.8297893
+    //         ]
+    //     },
+    //     "reportLocationString": "{ \"type\": \"Point\", \"coordinates\": [ 100.5647297, 13.8297893 ] }",
+    //     "remark": "",
+    //     "images": [
+    //         {
+    //             "report": 552459,
+    //             "guid": "c83eeaf8-ee58-4228-bff6-48006255bbea",
+    //             "imageUrl": "https://s3-ap-southeast-1.amazonaws.com/podd/c83eeaf8-ee58-4228-bff6-48006255bbea",
+    //             "thumbnailUrl": "https://s3-ap-southeast-1.amazonaws.com/podd/c83eeaf8-ee58-4228-bff6-48006255bbea-thumbnail",
+    //             "note": "",
+    //             "location": {}
+    //         }
+    //     ],
+    //     "firstImageThumbnail": "https://s3-ap-southeast-1.amazonaws.com/podd/c83eeaf8-ee58-4228-bff6-48006255bbea-thumbnail",
+    //     "createdByContact": "",
+    //     "administrationAreaAddress": "Thailand",
+    //     "administrationAreaName": "Thailand",
+    //     "formDataExplanation": "แจ้งเหตุสาธารณะ",
+    //     "renderedOriginalFormData": "แจ้งเหตุสาธารณะ",
+    //     "parentGuid": null,
+    //     "tags": [],
+    //     "isPublic": false,
+    //     "likeCount": 0,
+    //     "commentCount": 0,
+    //     "meTooCount": 0,
+    //     "likeId": null,
+    //     "meTooId": null,
+    //     "boundary": {
+    //         "top": 100.5547297,
+    //         "left": 13.8197893,
+    //         "right": 13.8397893,
+    //         "bottom": 100.5747297
+    //     },
+    //     "isCurated": false,
+    //     "canEdit": true
+    // }
+    //                 ],
+    //             }),
+    //         };
+    //     },
+    // };
+    /**
+     * @Query
+     *  - dateFrom: yyyy-mm-dd
+     *  - dateTo: yyyy-mm-dd
+     *  - page: int
+     *  - page_size: int
+     */
+    return $resource(
+      config.API_BASEPATH + "/civic/reports/:status/",
+      {
+        status: "@status",
+      },
+      {
+        query: {
+          method: "GET",
+          isArray: false,
+        },
+      }
+    );
+  });
